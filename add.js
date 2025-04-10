e4de7e9e3130ec058713b92651aa266e9683ed01
RainerMorales <jerenmae212212@gmail.com>
2025-04-10 12:20:54 +0800

    update

--- add.js
+++ add.js
@@ -2,7 +2,21 @@ setInterval(oras, 0);
 function oras() {
   const date1 = new Date();
   const date2 = date1.getFullYear();
-  const localtime = date1.toLocaleTimeString();
+  const hours = date1.getHours()
+
+  if (hours >= 5 && hours < 12) {
+    document.getElementById("good").innerHTML = "Good Morning! &#128513";
+  } else if (hours >= 12 && hours < 17) {
+   document.getElementById("good").innerHTML = "Good Afternoon! &#128526";
+  } else {
+    document.getElementById("good").innerHTML = "Good Evening! &#128564;";
+  }
+  const localtime = date1.toLocaleTimeString("en-GB", {
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
   document.getElementById("times").innerHTML = localtime;
   document.getElementById(
     "foots"
@@ -10,23 +24,31 @@ function oras() {
 }
 
 text1.addEventListener("keydown", function (v) {
-    const date1 = new Date();
-    const localtime = date1.toLocaleTimeString();
+  const date1 = new Date();
+  const localtime = date1.toLocaleTimeString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
   const text1 = document.getElementById("text1");
   if (v.key === "Enter") {
     if (text1 && text1.value.trim() === "") {
       window.alert("TYPE SOMETHING!");
     } else {
-      const text1 = document.getElementById("text1");
-      const ul = document.getElementById("ul");
-      const li = document.createElement("li");
-      li.innerHTML = `
-      <div class="bg-gray-900 p-4 flex justify-between">
-      <div>${text1.value}</div>
-      <div class="flex items-center text-sm">${localtime + " ago"}</div>
+      const text1 =document.getElementById("text1");
+      localStorage.setItem("value",text1.value)
+      const text2 = localStorage.getItem("value")
+      const li =document.createElement("li")
+      const ul =document.getElementById("ul")
+      li.innerHTML = `<div class="bg-gray-900 p-4 flex justify-between mt-2">
+      <div class=" text-left w-60">${text2}</div>
+      <div class="flex items-center text-sm opacity-50 w-40 justify-center">${
+        "sent " + localtime
+      }</div>
       </div>`;
-      text1.value = "";
-      ul.appendChild(li);
+      ul.appendChild(li)
+      text1.value=""
     }
   }
 });
+
